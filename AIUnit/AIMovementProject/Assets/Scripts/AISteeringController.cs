4a8dfd0d62c688aafc2ecfe136e2fa3d87b62e81
Joseph Huskey <s209062@students.aie.edu>
2021-02-11 18:01:40 -0800

    added basics to steering!

---
+++ AIUnit/AIMovementProject/Assets/Scripts/AISteeringController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISteeringController : MonoBehaviour
+{
+    public Agent agent;
+
+    [Header("Steering Settings")]
+    public float maxSpeed = 3.0f;
+    public float maxForce = 5.0f;
+}
+public class SteeringBehavior
+{
+    public virtual Vector3 Steer(AISteeringController controller) 
+    {
+        return Vector3.zero;
+    }
+}
+public class SeekSteering : SteeringBehavior 
+{
+    public Transform target;
+    public override Vector3 Steer(AISteeringController controller)
+    {
+        return (target.position - controller.transform.position).normalized * controller.maxSpeed;
+    }
+}
