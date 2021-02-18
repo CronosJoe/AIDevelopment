9a32616c7ac40bd9f65731ae24b9c44ebabb6403
Joseph Huskey <s209062@students.aie.edu>
2021-02-18 14:15:55 -0800

    Added more to behaviors gotta finish

--- AIUnit/AIMovementProject/Assets/Scripts/AISteeringController.cs
+++ AIUnit/AIMovementProject/Assets/Scripts/AISteeringController.cs
@@ -9,10 +9,35 @@ public class AISteeringController : MonoBehaviour
     [Header("Steering Settings")]
     public float maxSpeed = 3.0f;
     public float maxForce = 5.0f;
+    [Header("List of behaviours")]
+    List<SteeringBehavior> steerings = new List<SteeringBehavior>();
+    [Header("Important Transforms")]
+    public Transform seekTarget;
+    
+    protected Vector3 CalculateSteeringForce()
+    {
+        Vector3 steeringForce = Vector3.zero;
+        for (int i = 0; i < steerings.Count; i++) 
+        {
+            steeringForce += steerings[i].Steer(this);//adds a value to the steering force based on the return of the method steer, since the steering behaviours will inherit have their own Steer method
+        }
+        //clamp it
+        steeringForce = Vector3.ClampMagnitude(steeringForce, maxForce);
+        return steeringForce;
+    }
+    private void Start() //I don't see a world where I will ever "call" the Start method since it will be used when the program starts
+    {
+        steerings.Add(new SeekSteering { target = seekTarget }); //in the start we'd add all the different behaviours
+    }
+    private void Update()//another method that auto activates and shouldn't be called by me
+    {
+        Vector3 steeringForce = CalculateSteeringForce();
+        agent.velocity = Vector3.ClampMagnitude(agent.velocity + steeringForce, maxSpeed); //give em the clamps clamps
+    }
 }
 public class SteeringBehavior
 {
-    public virtual Vector3 Steer(AISteeringController controller) 
+    public virtual Vector3 Steer(AISteeringController controller)
     {
         return Vector3.zero;
     }
@@ -25,3 +50,34 @@ public class SeekSteering : SteeringBehavior
         return (target.position - controller.transform.position).normalized * controller.maxSpeed;
     }
 }
+public class FleeSteering : SteeringBehavior 
+{
+    public Transform target;
+    public override Vector3 Steer(AISteeringController controller)
+    {
+        return (target.position + controller.transform.position).normalized * controller.maxSpeed;
+    }
+}
+public class WanderSteering : SteeringBehavior 
+{
+    public Transform Randtarget;
+    public float allowedRadius = 10f;
+    public override Vector3 Steer(AISteeringController controller)
+    {
+        Vector3 minPosition = new Vector3(controller.agent.transform.position.x - allowedRadius, controller.agent.transform.position.y, controller.agent.transform.position.z - allowedRadius);
+        Vector3 maxPosition = new Vector3(controller.agent.transform.position.x + allowedRadius, controller.agent.transform.position.y, controller.agent.transform.position.z + allowedRadius);
+        Randtarget.position = new Vector3(Random.Range(minPosition.x, maxPosition.x), controller.agent.transform.position.y, Random.Range(minPosition.z, maxPosition.z));
+       // Randtarget.
+       //TODO finish this and pursue and do flock!
+        return (Randtarget.position - controller.transform.position).normalized * controller.maxSpeed;
+    }
+}
+public class PursueSteering : SteeringBehavior 
+{
+    public Transform target;
+    public override Vector3 Steer(AISteeringController controller)
+    {
+        return base.Steer(controller);
+    }
+}
+
---
+++ AIUnit/AIMovementProject/Assets/Scripts/FlockSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockSystem : MonoBehaviour
+{
+    public int boidCount = 100;
+    public float radius = 10f;
+    public float neighborhoodRadius = 5f;
+
+    public float mass = 1.0f;
+    public float maxForce = 5.0f;
+    public float maxSpeed = 10.0f;
+
+    public GameObject boidPrefab;
+    public Transform boidTarget;
+    private Transform[] boidTransform;
+    private Vector3[] boidPosition;
+    private Vector3[] boidVelocity;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        boidTransform = new Transform[boidCount]; //array with size of the amount of boids
+        boidPosition = new Vector3[boidCount];
+        boidVelocity = new Vector3[boidCount];
+
+        for(int i = 0; i<boidCount; i++)
+        {
+            GameObject newBoid = Instantiate(boidPrefab, transform.position + Random.insideUnitSphere * radius, transform.rotation);
+            boidTransform[i] = newBoid.transform;
+            boidPosition[i] = boidTransform[i].position;
+            boidVelocity[i] = boidTransform[i].forward;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
