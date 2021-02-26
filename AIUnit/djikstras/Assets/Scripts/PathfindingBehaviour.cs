d44af66e6634862d6f7d824d7c08de46642f819c
CronosJoe <CronosJoe@gmail.com>
2021-02-25 17:59:06 -0800

    gotta fix assessment pushy problem can't push the file up for some reason?

---
+++ AIUnit/djikstras/Assets/Scripts/PathfindingBehaviour.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfindingBehaviour : MonoBehaviour
+{
+    public int gridWidth;
+    public int gridHeight;
+    public Tile[] tiles;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    
+    private void Start()
+    {
+        tiles = new Tile[gridWidth * gridHeight];
+        Vector3 offset = Vector3.zero;
+        //spawn all the cubes
+        for(int i = 0; i < gridHeight; i++)
+        {
+            for(int j = 0; j < gridWidth; j++) 
+            {
+                GameObject newTile = Instantiate(prefab, transform.position + offset, transform.rotation);
+                tiles[i * gridWidth + j] = newTile.GetComponent<Tile>();
+                offset.x += 1.0f;
+            }
+            offset.x = 0f;
+            offset.z += 1.0f;
+        }
+    }
+    private int GetTileIndex(Tile target)
+    {
+        for(int i = 0; i < tiles.Length; i++) 
+        {
+            if (tiles[i] == target) 
+            {
+                return i;
+            }
+        }
+        return -1;//-1 means not found
+    }
+    private Tile GetCheapestTile(Tile[] arr)
+    {
+        float bestGSCore = float.MaxValue;
+        Tile bestTile = null;
+
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            if (arr[i].gScore < bestGSCore)
+            {
+                bestTile = arr[i];
+                bestGSCore = arr[i].gScore;
+            }
+        }
+        return bestTile;
+    }
+    
+    public Tile[] calculatePath(Tile origin, Tile destination) 
+    {
+        List<Tile> openList = new List<Tile>();
+        List<Tile> closedList = new List<Tile>();
+
+        openList.Add(origin);
+        while (openList.Count != 0 && !closedList.Contains(destination))
+        {
+            Tile current = GetCheapestTile(openList.ToArray());
+            openList.Remove(current);
+
+            closedList.Add(current);
+            int currentIdx = GetTileIndex(current);
+            //hardcoding cost of 1 since they spawn 1 unit away from each other
+            //left
+        }
+        return tiles;
+    }
+    
+ 
+}
---
+++ AIUnit/djikstras/Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Tile previousTile;
+    public Tile[] connectedTiles;
+    public int gScore;
+    public Tile right;
+    public Tile left;
+    public Tile top;
+    public Tile bottom;
+
+    public int CompareTo(Tile other) 
+    {
+        return -1;
+    }
+}
+
